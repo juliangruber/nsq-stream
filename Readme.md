1fbc2a5a922383f816ebc7d36049ba94bb029ce1
Julian Gruber <julian@juliangruber.com>
2014-06-07 13:27:19 +0200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,48 @@
+
+# nsq-stream
+
+  Streaming interface for nsq.
+
+## Example
+
+```js
+var nsqStream = require('nsq-stream');
+var nsq = require('nsq.js');
+
+var reader = nsq.reader(/* ... */);
+var writer = nsq.writer(/* ... */);
+
+var subscriber = nsqStream.createReadStream(reader);
+subscriber.on('data', function(json){
+  console.log(json);
+});
+
+var publisher = nsqStream.createWriteStream(writer, 'topic');
+publisher.write({ foo: 'bar' });
+```
+
+## API
+
+### createReadStream(reader[, type])
+
+  Create a readable stream from `reader`.
+
+  Type can be:
+
+  - `json`: `msg.json()` (default)
+  - `buffer`: `msg.body`
+  - `message`: `msg`
+
+### createWriteStream(writer, topic)
+
+  Create a writable stream from `writer` to `topic`.
+
+### Backpressure
+
+  Backpressure is created by not `.finish()`ing messages until they have
+  been consumed.
+
+## License
+
+  MIT
+
---
+++ index.js
@@ -0,0 +1,59 @@
+var Stream = require('readable-stream');
+var inherits = require('util').inherits;
+var assert = require('assert');
+
+exports.createReadStream = function(reader, type){
+  assert(reader, 'reader required');
+
+  var json = !type || 'json' == type;
+  var buffer = 'buffer' == type;
+  var message = 'message' == type;
+
+  var buf = [];
+  reader.on('message', function(msg){
+    buf.push(msg);
+  });
+
+  function next(fn){
+    if (buf.length) return fn(buf.shift());
+    reader.once('message', function(){
+      next(fn);
+    });
+  }
+
+  var objectMode = json || message;
+  var stream = Stream.Readable({ objectMode: objectMode });
+
+  stream._read = function(){
+    next(function(msg){
+      msg.finish();
+      var out = json ? msg.json()
+        : buffer ? msg.body
+        : msg;
+      stream.push(out);
+    });
+  };
+
+  stream.on('end', function(){
+    buf.forEach(function(msg){
+      msg.finish();
+    });
+    buf = [];
+  });
+
+  return stream;
+};
+
+exports.createWriteStream = function(writer, topic){
+  assert(writer, 'writer required');
+  assert(topic, 'topic required');
+
+  var stream = Stream.Writable({ objectMode: true });
+
+  stream._write = function(chunk, enc, done){
+    writer.publish(topic, JSON.stringify(chunk), done);
+  };
+
+  return stream;
+};
+
